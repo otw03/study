4fd65f9ede9c06140745eb42aefa5dcb74e3fdd4
otw03 <ypd01476@gmail.com>
2022-12-17 19:03:49 +0900

    batchScheduler update

--- codes/Nodejs/BatchScheduler/02_scheduler2.js
+++ codes/Nodejs/BatchScheduler/02_scheduler2.js
@@ -18,7 +18,7 @@ schedule.scheduleJob(rule2, () => logger.debug(`[rule2] 매시간 ${rule2.minute
 
 /** 4) 매일 시,분,초마다 수행 */
 const rule3 = new schedule.RecurrenceRule();
-rule2.hour = 17;
+rule3.hour = 17;
 rule3.minute = 22;
 rule3.second = 37;
 schedule.scheduleJob(rule3, () => logger.debug(`[rule3] 매일 ${rule3.hour}시 ${rule3.minute}분 ${rule3.second}초마다 수행!!`));
